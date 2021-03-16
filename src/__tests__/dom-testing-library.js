e206b25fd5e5b3edd3b0bedd699ebd3f86c885ca
James Rogers <jd2rogers2@gmail.com>
2021-03-16 10:40:14 -0700

    dom-testing-library donzo

--- src/__tests__/dom-testing-library.js
+++ src/__tests__/dom-testing-library.js
@@ -1,7 +1,10 @@
+// donzo
+
 import 'jest-dom/extend-expect'
 import React from 'react'
 import ReactDOM from 'react-dom'
 // 🐨 you'll need the getQueriesForElement function from 'dom-testing-library'
+import {getQueriesForElement} from 'dom-testing-library'
 // 📖 read more in the docs: https://github.com/kentcdodds/dom-testing-library/blob/7cb84a9068fd04d17d89edb8988fcc181a40becf/README.md#within-and-getqueriesforelement-apis
 import {FavoriteNumber} from '../favorite-number'
 
@@ -9,11 +12,14 @@ test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
   // use getQueriesForElement with the `div` to get the "getByLabelText" query
+  const {getByLabelText} = getQueriesForElement(div)
   // use the "getByLabelText" query to get the input, then you can
+  const input = getByLabelText('Favorite Number')
   // remove the assertion on the label.
   // 📖 read more in the docs: https://github.com/kentcdodds/dom-testing-library/blob/7cb84a9068fd04d17d89edb8988fcc181a40becf/README.md#getbylabeltext
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  // expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+  expect(input).toHaveAttribute('type', 'number')
+  // expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
 
 //////// Elaboration & Feedback /////////
